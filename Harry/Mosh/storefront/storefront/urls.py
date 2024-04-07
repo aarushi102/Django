e5039b39a025a16ab67566d122a0224797cb49a7
Aarushi <aarushiajay4@gmail.com>
2024-04-07 14:05:22 +0530

    Added the files

--- Harry/Mosh/storefront/storefront/urls.py
+++ Harry/Mosh/storefront/storefront/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for storefront project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    # path('about', views.about, name='about')
+    path('playground/', include('playground.urls'))
 ]
--- Harry/mysite/mysite/views.py
+++ Harry/mysite/mysite/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
+# def index(request):
+#     return HttpResponse(''' 
+#     Hello <h1>Aarushi Tiwari</h1>  <a href="https://www.youtube.com/watch?v=gnKbAAVUzro&list=PLCC34OHNcOtpalASMlX2HHdsLNipyyhbK&index=1"> 
+#     Hello Numpy playlist
+#     </a>''')
+
+
+# def about(request):
+#     return HttpResponse("About Aarushi")
+
 def index(request):
-    return HttpResponse(''' 
-    Hello <h1>Aarushi Tiwari</h1>  <a href="https://www.youtube.com/watch?v=gnKbAAVUzro&list=PLCC34OHNcOtpalASMlX2HHdsLNipyyhbK&index=1"> 
-    Hello Numpy playlist
-    </a>''')
-
-
-def about(request):
-    return HttpResponse("About Aarush")
+    return HttpResponse("home")
